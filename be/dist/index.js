593cc4e4188781b327751e4b27387f1f5dba1e81
Your New Name <vipinkumarti555@gmail.com>
2024-12-01 23:53:33 +0530

    currentily understanding the logic sorry for not writting the code

---
+++ be/dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+require("dotenv").config();
+const id = process.env.CLAUDE_API_KEY;
+console.log(id, 'id');
--- be/src/index.ts
+++ be/src/index.ts
@@ -1,2 +1,17 @@
+import Anthropic from "@anthropic-ai/sdk";
 require("dotenv").config();
 
+
+const anthropic = new Anthropic()
+
+async function main() {
+    const msg = await anthropic.messages.create({
+        model:"claude-3-5-haiku-20241022",
+        max_tokens:1000,
+        temperature:0,
+        messages:[]
+    })
+    console.log(msg);
+}
+
+main()
